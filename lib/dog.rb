ac245dc5e64fc61df567dcdedf9e69110ad94815
Rose Maina <rosewairimu27@gmail.com>
2022-08-24 19:17:00 +0300

    commit tree 458b8b2171e9fd4cf10129a86607e266c6ef25dd

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,12 @@
-class Dog
+require "pry"
+require "active_record"
+
+# Setup a database connection
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/dogs.sqlite"
+  )
+
+# Having the Dog class inherit from ActiveRecord::Base
+class Dog < ActiveRecord::Base
 end
